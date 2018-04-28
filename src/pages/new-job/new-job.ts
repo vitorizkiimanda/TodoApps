ac6e7a50d8761cc20790f55ff9f5a3794d5b563a
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-04-28 23:27:02 +0700

    first push

--- src/pages/new-job/new-job.ts
+++ src/pages/new-job/new-job.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { HelloIonicPage } from '../hello-ionic/hello-ionic';
 
-/**
- * Generated class for the NewJobPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-new-job',
   templateUrl: 'new-job.html',
